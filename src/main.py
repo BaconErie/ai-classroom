91b6ad5220b3a5b2ab43176dc6d14a0c6c0de0c3
BaconErie <baconerie0001@gmail.com>
2023-02-14 10:12:40 -0500

    basic server script

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return 'Signup'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login/classroom')
+def login_classroom():
+    return render_template('login_classroom.html')
+
+@app.route('/login/classroom/api')
+def login_api():
+    return {str(randint(1,19)): 'id'}
+
+@app.route('/login/classroom/<classroom_public_id>')
+def login_to_classroom(classroom_public_id):
+    return render_template('login_to_classroom.html', school_system='Proxy County Public Schools')
+
+if __name__ == '__main__':
+    app.run()
